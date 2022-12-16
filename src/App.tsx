3da38ec18e1f2d537a89770d9b667b6bc541c62a
Philip Bridges <you.dunno@gmail.com>
2022-12-16 10:58:06 -0500

    initial

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React, { ChangeEvent, useEffect, useState } from 'react'
+import Forecast from './components/Forecast'
+import Search from './components/Search'
+import useForecast from './hooks/useForecast'
+import { OptionType } from './types'
+
+const App = (): JSX.Element => {
+  const { term, options, forecast, onInputChange, onOptionSelect, onSubmit } =
+    useForecast()
+
+  return (
+    <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
+      {forecast ? (
+        <Forecast data={forecast} />
+      ) : (
+        <Search
+          term={term}
+          options={options}
+          onInputChange={onInputChange}
+          onOptionSelect={onOptionSelect}
+          onSubmit={onSubmit}
+        />
+      )}
+    </main>
+  )
+}
+
+export default App
